41f75eebd870c426213cacd320bbbd2286ce424d
Brian Durand <brian@embellishedvisions.com>
2010-06-23 15:01:41 -0500

    use in memory table for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,19 +5,13 @@ require 'sqlite3'
 module ActsAsRevisionable
   module Test
     def self.create_database
-      db_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'tmp'))
-      Dir.mkdir(db_dir) unless File.exist?(db_dir)
-      db = File.join(db_dir, 'test.sqlite3')
-      ActiveRecord::Base.establish_connection("adapter" => "sqlite3", "database" => db)
+      ActiveRecord::Base.establish_connection("adapter" => "sqlite3", "database" => ":memory:")
       ActsAsRevisionable::RevisionRecord.create_table
     end
 
     def self.delete_database
-      db_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'tmp'))
-      db = File.join(db_dir, 'test.sqlite3')
+      ActiveRecord::Base.connection.drop_table(ActsAsRevisionable::RevisionRecord.table_name)
       ActiveRecord::Base.connection.disconnect!
-      File.delete(db) if File.exist?(db)
-      Dir.delete(db_dir) if File.exist?(db_dir) and Dir.entries(db_dir).reject{|f| f.match(/^\.+$/)}.empty?
     end
   end
 end
